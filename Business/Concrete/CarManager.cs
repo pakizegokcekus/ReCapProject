d2bda6dc0f02aee9c497e0dc60fc959acf1f817d
Pakize Kuş <pakize.kuss@gmail.com>
2021-03-31 13:57:36 +0300

    Şart kodu eklendi.

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -9,26 +9,30 @@ using DataAccess.ReCap.Models;
 
 namespace Business.Concrete
 {
-    public class CarManager:ICarService
+    public class CarManager : ICarService
     {
         ICarDal _carDal;
 
         public CarManager(ICarDal carDal)
         {
             _carDal = carDal;
-           
+
         }
 
         public IResult Add(Car car)
         {
-            _carDal.Add(car);
-            return new Result(true, "");
+            if (car.CarName.Length >= 2 && car.DailyPrice >= 0)
+            {
+                _carDal.Add(car);
+                return new Result(true, "");
+            }
+            return new Result(false, "");
         }
 
         //public IDataResult<List<Car>> GetAll()
         //{
         //    var list = _carDal.GetAll().ToList();
-            
+
         //}
 
     }
